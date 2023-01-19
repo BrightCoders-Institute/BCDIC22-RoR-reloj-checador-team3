91a00ea39ee002fc9ab542a295e98cc59c7a6e71
M-Cordova-M <alberto971801@gmail.com>
2023-01-19 15:56:12 -0600

    create crud of stores
    we created a crud format for stores
    
    Co-authored-by: DiegoCoronaBarragan <d.coronabarragan@gmail.com>
    Co-authored-by: AMCernas <elswordmon@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
+  get 'stores/index'
+  get 'stores/show'
+  get 'stores/new'
+  get 'stores/edit'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root 'employees#index'
+  #root 'employees#index'
   resources :employees
+  
+  root 'stores#index'
+  resources :stores
+
 
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_19_032548) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_19_214403) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_19_032548) do
     t.string "position"
     t.string "employee_num"
     t.string "employee_private_num"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "stores", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
