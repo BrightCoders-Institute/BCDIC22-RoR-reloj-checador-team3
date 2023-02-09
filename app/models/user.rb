a1d109931c8908916d8c6357da89b0950dd745c2
Arnoldo Magaña <elswordmon@gmail.com>
2023-02-08 19:13:46 -0600

    modify for deploy errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-         u = User.new(:email => "admin@example.com", :password => 'password', :password_confirmation => 'password')
-         u.save
 end
