f6a7423808ca284cf8505b90047372499c5fbe76
Arnoldo Magaña <elswordmon@gmail.com>
2023-02-09 12:02:01 -0600

    delete files

--- db/migrate/20230130221325_add_columns.rb
+++
@@ -1,7 +0,0 @@
-class AddColumns < ActiveRecord::Migration[7.0]
-  def change
-    add_column :employees, :employee_num, :integer
-    add_column :employees, :employee_private_num, :integer
-  end
-end
-
